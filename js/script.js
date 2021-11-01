8ecd6cc2a92c4769c2397d3194a814510c39866c
Александр Мельников <91779711+Aleksandr232@users.noreply.github.com>
2021-11-01 22:11:46 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('.carousel_inner').slick({
         speed: 1300,
         autoplay: true,
-        autoplaySpeed: 1000,
+        autoplaySpeed: 1200,
         prevArrow: '<button type="button" class="slick-prev"><img src="slider/chevron/chevron right solid.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="slider/chevron/chevron left solid.png"></button>',
         responsive: [

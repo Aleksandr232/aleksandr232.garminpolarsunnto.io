599b597bd0a47350ed56bd34b45a0bbb679c612e
Александр Мельников <91779711+Aleksandr232@users.noreply.github.com>
2021-11-05 21:21:33 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,113 +1,120 @@
 $(document).ready(function(){
-    $('.carousel_inner').slick({
-        speed: 1300,
-        autoplay: true,
-        autoplaySpeed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img class="right" src="slider/chevron/chevron right solid.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img class="left" src="slider/chevron/chevron left solid.png"></button>',
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [
-            {  
-                breakpoint: 768,
+            {
+                breakpoint: 992,
                 settings: {
-                dots: false,
-                arrows:false
+                    dots: true,
+                    arrows: false
                 }
             }
-        ]   
-    });
-    $('ul.tabs').on('click', 'li:not(.catalog__tab_active)', function() {
-        $(this)
-          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active')
-          .eq($(this).index()).addClass('catalog__content_active');
+        ]
     });
     
-    function toggleSlide(item){
-        $(item).each(function(i){
-            $(this).on('click', function(e){
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            });
+            })
         });
     };
 
-toggleSlide('.catalog-item__link');
-toggleSlide('.catalog-item__back');
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 
-//modal
+    // Modal
 
-    $('[data-modal=consultation]').on('click', function(){
-    $('.overlay, #consultation').fadeIn('slow');
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
     });
-    $('.modal__close').on('click', function(){
-        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
     });
 
-    $('.button_mini').each(function(i){
-        $(this).on('click', function(){
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
             $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                  },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Неправильно введен адрес почты"
+                }
+            }
         });
+    };
 
-    });
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
 
-    $('#feed_form_consu').validate({
-        rules:{
-            name:'required',
-            phone:'required',
-            email: {
-                required: true,
-                email: true
-            }
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
 
-        },
-        messages:{
-            name:'Пожалуйста, введите имя',
-            maxlength:'Максимальное количиство символов 15',
-            phone:{
-                required: 'Пожалуйста, введите номер',
-                phone:'Неправильно введен номер !',
-                minlength:'Минимум 11 символов'
-            },    
-            email:{
-                 required: 'Пожалуйста, введите email',
-                 email:'Неправильно введен email !'
-            }
-        }
-    });
-
-
-    $('form').submit(function(e){
+    $('form').submit(function(e) {
         e.preventDefault();
         $.ajax({
-            type:'POST',
-            URL: "mailer/smart.php",
+            type: "POST",
+            url: "mailer/smart.php",
             data: $(this).serialize()
         }).done(function() {
-            $(this).find('input').val('');
-            $('#cosultattion, #order').fadeOut();
-            $('.overlay, #thanks').fadeIn("slow");
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
 
             $('form').trigger('reset');
         });
         return false;
     });
 
-    //smooth scroll and page up
+    // Smooth scroll and pageup
 
     $(window).scroll(function() {
         if ($(this).scrollTop() > 1600) {
             $('.pageup').fadeIn();
         } else {
-            ('.pageup').fadeOut();
-        }        
+            $('.pageup').fadeOut();
+        }
     });
 
-    $("a[href^='#']").click(function(){
+    $("a[href=#up]").click(function(){
         const _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
         return false;
     });
 
+    new WOW().init();
 });
+
